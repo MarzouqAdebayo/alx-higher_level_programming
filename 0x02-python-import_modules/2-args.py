c577c21446d6d451add8d21fcaf822eceb0a4937
marzouq <marzouqaadebayo@gmail.com>
2023-03-09 18:44:47 +0000

    bug fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,10 +10,10 @@ def print_argv():
 
 if __name__ == "__main__":
     if len(argv) - 1 == 1:
-        print("{} argument".format(len(argv) - 1))
+        print("{} argument:".format(len(argv) - 1))
         print_argv()
     elif len(argv) - 1 == 0:
-        print("{} arguments".format(len(argv) - 1))
+        print("{} arguments.".format(len(argv) - 1))
     else:
-        print("{} arguments".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
         print_argv()
