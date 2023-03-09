4009f47e5bcebd2b474fdc4a698d57981d1b2aed
marzouq <marzouqaadebayo@gmail.com>
2023-03-09 18:22:13 +0000

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,19 +2,19 @@
 from sys import argv
 
 def print_argv():
-	i = 0
-	for j in argv:
-		if j == 0:
-			continue
-		print(f"{i}: {j}")
-		i += 1
+    i = 0
+    for j in argv:
+        if j == 0:
+            continue
+        print(f"{i}: {j}")
+        i += 1
 
 if __name__ == "__main__":
-	if len(argv) - 1 == 1:
-		print(f"{len(argv) - 1} argument")
-		print_argv()
-	elif len(argv) - 1 == 0:
-		print(f"{len(argv) - 1} arguments")
-	else:
-		print(f"{len(argv) - 1} arguments")
-		print_argv()
+    if len(argv) - 1 == 1:
+        print(f"{len(argv) - 1} argument")
+        print_argv()
+    elif len(argv) - 1 == 0:
+        print(f"{len(argv) - 1} arguments")
+    else:
+        print(f"{len(argv) - 1} arguments")
+        print_argv()
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,14 +2,14 @@
 from sys import argv
 
 def infinite_add():
-	if len(argv) - 1 < 1:
-		print("0")
-	else:
-		sum, i = 0, 1
-		while i < len(argv):
-			sum += int(argv[i])
-			i += 1
-		print(sum)
+    if len(argv) - 1 < 1:
+        print("0")
+    else:
+        sum, i = 0, 1
+        while i < len(argv):
+            sum += int(argv[i])
+            i += 1
+        print(sum)
 
 if __name__ == "__main__":
-	infinite_add()
+    infinite_add()
