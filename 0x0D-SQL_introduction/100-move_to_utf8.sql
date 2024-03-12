4220e6013c8e2d2a7e22296ec2f8cbb368ba3a94
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-12 14:45:06 +0000

    Fixing bug in task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,7 +4,7 @@ ALTER DATABASE hbtn_0c_0
   COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE hbtn_0c_0.first_table
-  CONVERT TO CHARACTER SET utf8mb4
+  CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE hbtn_0c_0.first_table
