2527bd13b952b2cefe6838aa7914be2cead6b412
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-14 02:54:29 +0000

    Bug fix in task 17

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@ SELECT name FROM tv_genres
 WHERE name NOT IN (
   SELECT tv_genres.name FROM tv_shows
   INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-  INNER JOIN tv_genres ON tv_genres.genre_id = tv_genres.id
+  INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
   WHERE tv_shows.title = 'Dexter'
 )
-ORDER BY name ASC
+ORDER BY name ASC;
