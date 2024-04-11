7782fefd574079533b6bf471623ea67903a17783
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-04-11 02:45:50 +0000

    Committing file 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
+  value: 12
 };
 console.log(myObject);
 
@@ -16,4 +16,3 @@ myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
