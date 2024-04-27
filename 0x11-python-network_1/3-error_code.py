933bf44c8b7790af105bebe0c7539542a4ce0ed9
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-04-27 07:00:15 +0000

    Committing file 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+"""
+from urllib import request, error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = request.Request(url)
+
+    try:
+        with request.urlopen(req) as response:
+            res = response.read().decode('ascii')
+            print(re)
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
