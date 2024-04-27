59c62d325c66bd5b5904a54685db801ca6831ae6
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-04-27 06:09:24 +0000

    Fixed pycodestyle errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,10 @@ A Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
         res = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(res)))
         print("\t- content: {}".format(res))
-        print("\t- utf8 content: {}".format(res.decode(encoding='utf-8')))
+        print("\t- utf8 content: {}".format(res.decode(encoding='utf-8')))"
