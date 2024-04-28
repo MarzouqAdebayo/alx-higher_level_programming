edbaf0bad26b39346c77383167c3b0c3423823ae
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-04-28 02:36:01 +0000

    Committing file 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,13 +3,15 @@
 Python script that takes 2 arguments (repository and user) and gets the
 last 10 commits
 """
-import requests, sys
+import requests
+import sys
 
 
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    headers = {'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28'}
+    headers = {'Accept': 'application/vnd.github+json',
+               'X-GitHub-Api-Version': '2022-11-28'}
     r = requests.get(
             "https://api.github.com/repos/{}/{}/commits".format(
                 repo_name, owner_name), headers=headers)
