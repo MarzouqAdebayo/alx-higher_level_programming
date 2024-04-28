9d6cd9bf58db21808beee525285b02d07c095b66
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-04-28 01:06:03 +0000

    Committing file 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
-    data = url.parse.urlencode(values).encode('ascii')
+    data = urllib.parse.urlencode(values).encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         res = response.read().decode('utf-8')
