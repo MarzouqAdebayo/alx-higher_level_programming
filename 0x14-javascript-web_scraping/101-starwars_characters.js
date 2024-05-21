d9de96a95e394699707228e86582e91462496136
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-05-21 20:52:15 +0000

    Fixed bug

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,13 +1,21 @@
 #!/usr/bin/node
 const req = require('request');
+function getUsers(chs, idx = 0) {
+	if (idx >= chs.length) return;
+	console.log(chs.length, idx);
+	req(chs[idx], (err, res, data) => {
+		if (!err) {
+			console.log(JSON.parse(data).name);
+			getUsers(chs, idx + 1);
+		}
+	});
+}
 req(
-    `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
-    (err, res, data) => {
-      if (err) console.log(err);
-      else {
-        JSON.parse(data).characters.forEach((ch) => {
-          req(ch, (err, res, data) => console.log(err || JSON.parse(data).name));
-        });
-      }
-    }
+	`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+	(err, res, data) => {
+		if (err) console.log(err);
+		else {
+			getUsers(JSON.parse(data).characters);
+		}
+	}
 );
