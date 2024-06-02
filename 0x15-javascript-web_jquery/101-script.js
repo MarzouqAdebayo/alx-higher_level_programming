ce991f6f2138cf965f65ac9100faa0ea5ce76810
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-06-02 08:11:04 +0000

    Committing file 101-script.js

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+$(function () {
+  const ul = $('UL.my_list');
+  $('DIV#add_item').on('click', function () {
+    ul.append('<li>Item</li>');
+  });
+  $('DIV#remove_item').on('click', function () {
+    $('UL.my_list>li:last-child').remove();
+  });
+  $('DIV#clear_list').on('click', function () {
+    ul.empty();
+  });
+});
