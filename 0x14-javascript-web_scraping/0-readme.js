9858a22f1c500942017c130ca046456358034bdf
Marzouq Adebayo <117763013+MarzouqAdebayo@users.noreply.github.com>
2024-06-27 08:03:35 +0100

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, data) => console.log(err || data));
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const req = require('request');
-req('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, res) => console.log(err || JSON.parse(res.body).title));
